f01357065d1174fe6817ead7fd2d0380a30b798c
caseyfoster <me@caseyfoster.net>
2022-12-14 20:49:04 -0500

    yay6

--- main.js
+++ main.js
@@ -1,28 +1,31 @@
 //getting item
-let caseyName = document.getElementById('casey');
+// let hoverMe = document.getElementById('hover-me');
+
 
 //function to change size
 // let changeText = function() {
 //     caseyName.innerHTML = "Hovered";
 // }
 
-let changeBack = function() {
-    caseyName.innerHTML = "Unhovered"
-}
-
+let opacity = document.getElementById('opacity');
+let hoverText = document.getElementById('hover-me');
 let image = document.getElementById('img');
 
 let changeImage = function() {
     image.style.opacity = "30%";
-    caseyName.innerHTML = "30% Opacity";
+    opacity.innerHTML = "30% Opacity";
+    hoverText.style.backgroundColor = 'darkgray';
+    hoverText.style.color = 'white';
 }
 
 let changeImageBack = function() {
     image.style.opacity = "100%";
-    caseyName.innerHTML = "100% Opacity";
+    opacity.innerHTML = "100% Opacity";
+    hoverText.style.backgroundColor = '';
+    hoverText.style.color = '';
 }
 
 // caseyName.addEventListener('mouseover', changeText);
 // caseyName.addEventListener('mouseout', changeBack);
-image.addEventListener('mouseover', changeImage);
-image.addEventListener('mouseout', changeImageBack)
+hoverText.addEventListener('mouseover', changeImage);
+hoverText.addEventListener('mouseout', changeImageBack)
